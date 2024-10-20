564b0081b81cc14d0270de0478d4f535010e1d82
samuel <chyken07@gmail.com>
2024-10-20 08:46:16 -0700

    new changes

--- server/client/vite.config.js
+++ server/client/vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
-// https://vitejs.dev/config/
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   build: {
-    outDir: '../dist',
+    outDir: '../server/public',
+    emptyOutDir: true
   },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
