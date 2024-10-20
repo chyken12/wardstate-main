f2fa3181eccc98fffa3790172f6660f8d145296c
samuel <chyken2022@outlook.com>
2024-10-20 22:43:59 +0000

    restructered app

--- server/client/vite.config.js
+++ server/client/vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../server/public',
-    emptyOutDir: true
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
 })
